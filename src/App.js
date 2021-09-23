3b463ae7816c23de7eb581a8de101df2a308916b
justinchow3010 <74143516+justinchow3010@users.noreply.github.com>
2021-09-23 22:06:05 +0800

    Finish basics look of the page.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,109 @@
 import './sass/App.scss';
 import ContainerBox from './components/ContainerBox';
+import { createRef, useEffect, useState } from 'react';
 
 function App() {
+  const personalDescription = `A final year student at CUHK, majoring in Information Engineering.\nPassionate about learning and changing things around.`;
+  const titleBoxes = [];
+  const backgroundImages = [];
+  const peopleImges = [];
+  const homeRef = createRef();
+  const projectRef = createRef();
+  const contactRef = createRef();
+  const [currentY, setCurrentY] = useState(window.pageYOffset);
+  const [lastY, setLastY] = useState(0);
+  const [scrollUp, setScrollUp] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', updateCurrentY)
+    return () => {
+      window.removeEventListener('scroll', updateCurrentY)
+    }
+  }, [])
+
+  useEffect(() => {
+    // console.log(currentY)
+    // console.log(projectRef.current.getBoundingClientRect().bottom)
+    if (lastY < currentY) {
+      setScrollUp(false)
+    } else {
+      setScrollUp(true)
+    }
+
+    setLastY(currentY <= 0 ? 0 : currentY);
+    console.log(scrollUp)
+    if (currentY <= homeRef.current.getBoundingClientRect().bottom && currentY >= homeRef.current.getBoundingClientRect().bottom - 30 && scrollUp === false) {
+      window.scrollBy({
+        top: projectRef.current.getBoundingClientRect().top - 100,
+        behavior: "smooth"
+      });
+    } else if (currentY <= projectRef.current.getBoundingClientRect().bottom && currentY >= projectRef.current.getBoundingClientRect().bottom - 30 && scrollUp === false) {
+      window.scrollBy({
+        top: contactRef.current.getBoundingClientRect().top - 100,
+        behavior: "smooth"
+      });
+      // } else if (currentY <= projectRef.current.getBoundingClientRect().bottom + 30 && currentY >= projectRef.current.getBoundingClientRect().bottom && scrollUp === true) {
+      //   console.log('asc')
+      //   window.scrollBy({
+      //     top: projectRef.current.getBoundingClientRect().top - 100,
+      //     behavior: "smooth"
+      //   });
+      // } else if (currentY <= homeRef.current.getBoundingClientRect().bottom + 30 && currentY >= homeRef.current.getBoundingClientRect().bottom && scrollUp === true) {
+      //   window.scrollBy({
+      //     top: homeRef.current.getBoundingClientRect().top - 100,
+      //     behavior: "smooth"
+      //   });
+    }
+  }, [currentY])
+
+
+  const toolBar = () => (
+    <header className="toolbar">
+      <div className="title" onClick={() => scrollToView(homeRef)}>
+        Justin Chow
+      </div>
+      <div className="toolbar-info">
+        <div className="toolbar-item" onClick={() => scrollToView(projectRef)}>
+          Project
+        </div>
+        <div className="toolbar-item" onClick={() => scrollToView(contactRef)}>
+          Contact
+        </div>
+      </div>
+    </header>
+  )
+
+  const bottomBar = () => (
+    <div className="bottom-bar">
+      Be what you want to be.
+    </div>
+  )
+
+  const updateCurrentY = () => {
+    setCurrentY(window.pageYOffset);
+  }
+
   return (
-    <>
-      {toolBar()}
-      <ContainerBox/>
-    </>
+    <div>
+      <div className="main-container">
+        {toolBar()}
+        <ContainerBox title="JUSTIN CHOW" description={personalDescription} titleBox="Home" background="bg1.svg" peopleImg="man with map.svg" ref={homeRef} />
+        <ContainerBox title="Repositories" description="Check out my repositories here!" titleBox="Project" background="bg10.svg" peopleImg="man with paper.svg" button={true} ref={projectRef} />
+        <ContainerBox title="Contact me" description={`Looking for new challenges${"\n"}Email: justinchow3010@gmail.com`} titleBox="Contact" background="bg19.svg" peopleImg="woman talking.svg" ref={contactRef} />
+      </div>
+      {bottomBar()}
+    </div>
   );
 }
 
-const toolBar = () => (
-  <header className="toolbar">
-    <div className="title">
-      Justin Chow
-    </div>
-    <div className="toolbar-info">
-      <div className="toolbar-item">
-        Project
-      </div>
-      <div className="toolbar-item">
-        Contact
-      </div>
-    </div>
-  </header>
-)
+const scrollToView = (ref) => {
+  const elementPosition = ref.current.getBoundingClientRect().top;
+  if (ref.current) {
+    window.scrollBy({
+      top: elementPosition - 100,
+      behavior: "smooth"
+    });
+  }
+}
+
 export default App;
--- src/components/ContainerBox.js
+++ src/components/ContainerBox.js
@@ -1,29 +1,55 @@
+import { forwardRef } from 'react';
 import '../sass/App.scss';
 
-const ContainerBox = () => {
+const ContainerBox = forwardRef(({ title = "JUSTIN CHOW", description = "Not available", titleBox = "HOME", background = "bg1.svg", peopleImg = "man with map.svg", button = false }, ref) => {
     return (
-        <div className="container-box">
+        <div className="container-box" ref={ref}>
             <div className="background-box">
                 <div className="trans-box">
                     <div>
                         <div className="name">
-                            JUSTIN CHOW
+                            {title}
                         </div>
                         <div className="description">
-                            CUHK student
+                            {description}
                         </div>
+                        {button === true
+                            ? <a className="button" href="https://github.com/justinchow3010?tab=repositories">
+                                more...
+                            </a>
+                            : <></>
+                        }
+                        {titleBox === "Contact"
+                            ? <div className="icon-container">
+                                <a className="icon" href="https://www.linkedin.com/in/justin-chow-41638b1bb/">
+                                    <img src={require("../assets/linkedin.svg").default} alt="lnkedin" style={{ width: "54px" }} />
+                                </a>
+                                <a className="icon" href="https://github.com/justinchow3010">
+                                    <img src={require("../assets/github.png").default} alt="lnkedin" style={{ width: "54px" }} />
+                                </a>
+                            </div>
+                            : <></>
+                        }
                     </div>
                 </div>
             </div>
-            <div>
-                <img src={require("../assets/bg1.svg").default} alt="background" className="background" />
-                <img src={require("../assets/man with map.svg").default} alt="man" className="people" />
+            <div className="image-box">
+                {background === "bg10.svg"
+                    ? <img src={require(`../assets/${background}`).default} alt="background" className="background" style={{ top: "-120px" }} />
+                    : background === "bg19.svg"
+                        ? <img src={require(`../assets/${background}`).default} alt="background" className="background" style={{ bottom: "90px", height: "600px" }} />
+                        : <img src={require(`../assets/${background}`).default} alt="background" className="background" />
+                }
+                {peopleImg === "woman talking.svg"
+                    ? <img src={require(`../assets/${peopleImg}`).default} alt="woman" className="people" style={{ left: "630px" }} />
+                    : <img src={require(`../assets/${peopleImg}`).default} alt="man" className="people" />
+                }
                 <div className="title-box">
-                    HOME
+                    {titleBox}
                 </div>
             </div>
         </div>
     )
-}
+})
 
 export default ContainerBox;
